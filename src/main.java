273f9ec55cb69cc03cd96ac7556605cd03a2c2a9
yusuke shimabukuro <e185723@ie.u-ryukyu.ac.jp>
2019-01-17 15:52:11 +0900

    最初のコミット

--- src/main.java
+++ src/main.java
@@ -1,11 +1,7 @@
 public class main {
     public static void main(String args[]) {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("例外が発生しました。");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        int value;
+        value = value = Integer.parseInt(str);
     }
 }
